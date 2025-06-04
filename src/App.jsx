adfd4833cb9f53b161176128744d467fb811e064
Uttam Pratap Singh <Uttampratap762@gmail.com>
2025-06-04 12:45:08 +0530

    done1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero";
 import Locations from "./components/Locations";
 import Quick from "./components/Quick";
 import Stats from "./components/Stats";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Locations/>
       <Stats/>
       <Footer/>
+      <SpeedInsights />
     </div>
   )
 }
